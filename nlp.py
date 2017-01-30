38c208fc142611c9068eafa0994eb6b633ff487f
sgp715 <7SebastianPerez@gmail.com>
2017-01-29 19:25:13 -0600

    added cli

---
+++ nlp.py
@@ -0,0 +1,32 @@
+import sys, os
+from preprocess import process_data_file
+
+def usage_message():
+    print "usage:"
+    print "python nlp.py -host <path/to/tab/file>"
+    print "python nlp.py -winner <path/to/tab/file>"
+    print "python nlp.py -presenters <path/to/tab/file>"
+
+if __name__ == "__main__":
+
+
+
+    args = sys.argv[1:]
+
+    if len(args) == 2:
+
+        # if we haven't preprocessed do so
+        if not(os.path.isfile('data_preprocessed.p')):
+            process_data_file(args[1])
+
+        if args[0] == "-host":
+            print("Find host")
+            exit()
+        elif args[0] == "-winner":
+            print("Find winners")
+            exit()
+        elif args[0] == "-presenters":
+            print("Find presenters")
+            exit()
+
+    usage_message()
