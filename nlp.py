543f3aa281f60eb620aefe41f97766e297963cda
Safia Abdalla <safia@safia.rocks>
2017-02-12 20:21:01 -0600

    Update entry point script

--- nlp.py
+++ nlp.py
@@ -2,13 +2,14 @@ import sys, os
 from preprocess import process_data_file
 from presenters import main
 from nominees import findnominees
-
+from award_winners import find_award_winners
+from sentiment_analysis import get_sentiment
 
 def usage_message():
-    print "usage:"
-    print "python nlp.py -nominees <path/to/tab/file>"
-    print "python nlp.py -winner <path/to/tab/file>"
-    print "python nlp.py -presenters <path/to/tab/file>"
+    print("usage:")
+    print("python nlp.py -nominees <path/to/tab/file>")
+    print("python nlp.py -winner <path/to/tab/file>")
+    print("python nlp.py -presenters <path/to/tab/file>")
 
 
 if __name__ == "__main__":
@@ -22,13 +23,18 @@ if __name__ == "__main__":
             process_data_file(args[1])
 
         if args[0] == "-nominees":
-            print("Nominees: ", findnominees()
+            print("Nominees: ", findnominees())
             exit()
         elif args[0] == "-winner":
             print("Find winners")
+            find_award_winners()
             exit()
         elif args[0] == "-presenters":
             print("Presenter: ", main())
+            exit()
+        elif args[0] == "-sentiment":
+            filter_word = args[1]
+            get_sentiment(filter_word)        
             exit()
 
     usage_message()
