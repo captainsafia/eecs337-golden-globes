0fd9aeb5b355f2fa7e617bbd4c7b1e5253d0ef1c
sgp715 <7SebastianPerez@gmail.com>
2017-02-13 11:17:02 -0600

    presenters

--- presenters.py
+++ presenters.py
@@ -5,21 +5,32 @@ from nltk.tag.stanford import StanfordNERTagger
 st = StanfordNERTagger('stanford-ner/english.all.3class.distsim.crf.ser.gz', 'stanford-ner/stanford-ner.jar')
 import collections
 import numpy as np
+import re
+import time
 
 
-def find_tweets_with_keyword(keyword, df):
+def find_tweets_with_keyword(keywords, df):
 
-    present_tweets = df.loc[df['TweetText'].str.contains(keyword).fillna(False)]["TweetText"]
+    for k in keywords:
+        df = df.loc[df['TweetText'].str.contains(k).fillna(False)]
+
+    return df["TweetText"]
+
+def find_bigrams(present_tweets):
+
     tweet_unigrams = [t.split(' ') for t in present_tweets]
     tweet_bigrams = [list(bigrams(t)) for t in tweet_unigrams]
 
-    return tweet_unigrams, tweet_bigrams
+    return tweet_bigrams
+
 
 def get_bg_frequency_dataframe(tweet_bigrams):
+
+    # TODO: optimize by using -> http://pandas.pydata.org/pandas-docs/stable/generated/pandas.Series.value_counts.html
     bg_count = {}
     for bg_row in tweet_bigrams:
         for bg in bg_row:
-            bg_i = bg[0]+' '+bg[1]
+            bg_i = bg[0] + ' ' + bg[1]
             if bg_i in bg_count:
                 bg_count[bg_i] += 1
             else:
@@ -27,31 +38,83 @@ def get_bg_frequency_dataframe(tweet_bigrams):
 
     return pd.DataFrame(list(bg_count.items()), columns=["bigrams","frequency"])
 
+def load_file(filename):
 
-def get_most_frequent_person(bg_df):
+    lasts = []
+    with open(filename,'r') as f:
+
+        line = f.readline()
+        while line:
+            lasts.append(line.strip())
+            line = f.readline()
+
+    return lasts
 
 
-    while bg_df.shape[0] > 0:
+def get_most_frequent_person(bg_df, num=50):
 
-        max_index = bg_df["frequency"].idxmax()
-        host = bg_df.loc[max_index]["bigrams"]
-        print(host)
-        bg_df.drop(max_index, inplace=True)
-        tag = st.tag([host])[0][1]
-        print("tag: ", tag)
-        if tag == "PERSON":
-            break
+    p = re.compile(r'([A-Z][a-z]+\s[A-Z][a-z]+)+')
+    bg_df["bigrams"] = bg_df["bigrams"].map(p.findall)
 
-    print("Host!", host)
-    return host
+    firsts = load_file("firstnames.txt")
+    lasts = load_file("surnames.txt")
+
+    peeps = []
+    for index, val in bg_df["bigrams"].iteritems():
+
+        if len(val) == 0:
+            continue
+
+        val = val[0]
+
+        names_list = val.split(' ')
+        if names_list[0].upper() not in firsts:
+            continue
+
+        if names_list[1].upper() not in lasts:
+            continue
+
+        peeps.append(val)
+
+    # while (bg_df.shape[0] > 0) and (num != 0):
+
+        # max_index = bg_df["frequency"].idxmax()
+        # host = bg_df.loc[max_index]["bigrams"]
+        # bg_df.drop(max_index, inplace=True)
+        # if host == []:
+        #     continue
+        # tag = st.tag(host)[0][1]
+        # if tag == "PERSON":
+        #     num -= 1
+        #     peoples.append(host[0])
+        #     print("found: ", peoples)
+
+    return peeps
+
 
 def main():
 
-    data_file_path = 'data_preprocessed.p'
+    data_file_path = 'p_data_preprocessed.p'
     df = pd.read_pickle(data_file_path)
 
-    tweet_bigrams = find_tweets_with_keyword("presenter", df)[1]
-    bg_df = get_bg_frequency_dataframe(tweet_bigrams)
-    person = get_most_frequent_person(bg_df)
+    tweets = find_tweets_with_keyword(["present", "best"], df)
+    bigrams = find_bigrams(tweets)
+    frequencies = get_bg_frequency_dataframe(bigrams)
+    return get_most_frequent_person(frequencies)
 
-    return person
+if __name__ == "__main__":
+
+    presenters = main()
+    print("Presenters: ", presenters)
+
+    correct = load_file("presenters.txt")
+
+    count = 0
+    for p in presenters:
+
+        if p in correct:
+            count += 1
+
+    print(count)
+    print(len(correct))
+    print(len(presenters))
